13496cac2f0c3de647fb6cfa4322f7f4655390e2
selistym <selistym@protonmail.com>
2019-01-10 08:54:37 -0800

    reset svg

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var level0 = [];
+
 
 // on window resize
 $(window).resize(function () {
@@ -22,70 +22,95 @@ var tool_tip = d3.tip()
   .offset([-8, 0])
   .html(function (d) { return d; });
 
-// load the external svg from a file
-jQuery.ajax({
-  dataType: "json",
-  url: "mapinfo/level0.json",
-  async: false,
-  success: function (data) { level0 = data; }
-});
 
-level0
-  .forEach(function (item) {
-    d3.xml("mapinfo/level0/" + item.id + ".svg").mimeType("image/svg+xml").get(function (error, xml) {
-      if (error) throw error;
+d3.xml("mapinfo/level0.svg").mimeType("image/svg+xml").get(function (error, xml) {
+  if (error) throw error;
 
-      var importedNode = document.importNode(xml.documentElement, true);
-      importedNode.id = item.id;
-      svg
-        .each(function () {
-          this.appendChild(importedNode);
-        })
-        
-      svg.select('svg#' + importedNode.id).call(tool_tip);
-      svg.select('svg#' + importedNode.id)
-        .attr("x", item.x)
-        .attr("y", item.y)
-        .attr("width", 100)
-        .attr("height", 100)
-        .on('mouseover', function () {
-          svg.select('svg#' + importedNode.id)
-            .style('cursor', 'pointer')
-            .style('fill-opacity', 0.3);
+  var importedNode = document.importNode(xml.documentElement, true);
+  // importedNode.id = item.id;
+  svg
+    .each(function () {
+      this.appendChild(importedNode);
+    })
 
-          tool_tip.show(importedNode.id);
-        })
-        .on('mouseout', function () {
-          svg.select('svg#' + importedNode.id)
-            .style('fill-opacity', 1);
-          tool_tip.hide();
-        })
-
-      //styleImportedSVG(importedNode.id);
-    });
-  })
-
-function styleImportedSVG(c_id) {
-  //console.log(svg.selectAll('svg'));
-  // .filter(function(c_svg){ console.log(c_svg); return c_svg.id == c_id})
-  // .attr("width", 50)
-  // .attr("height", 50);
-  // d3.selectAll('path').call(tool_tip);
-  // d3.selectAll('path')
+  // svg.select('svg#' + importedNode.id).call(tool_tip);
+  // svg.select('svg#' + importedNode.id)
+  //   .attr("x", item.x)
+  //   .attr("y", item.y)
+  //   .attr("width", 100)
+  //   .attr("height", 100)
   //   .on('mouseover', function () {
-  //     d3.selectAll('path')
+  //     svg.select('svg#' + importedNode.id)
   //       .style('cursor', 'pointer')
-  //       .style('fill-opacity', 0.1);
-  //     d3.selectAll('polygon')
-  //       .style('cursor', 'pointer')
-  //       .style('fill-opacity', 0.1);
-  //     tool_tip.show();
+  //       .style('fill-opacity', 0.3);
+
+  //     tool_tip.show(importedNode.id);
   //   })
   //   .on('mouseout', function () {
-  //     d3.selectAll('path')
-  //       .style('fill-opacity', 1);
-  //     d3.selectAll('polygon')
+  //     svg.select('svg#' + importedNode.id)
   //       .style('fill-opacity', 1);
   //     tool_tip.hide();
   //   })
-}
+
+  //styleImportedSVG(importedNode.id);
+});
+
+// level0
+//   .forEach(function (item) {
+//     d3.xml("mapinfo/level0/" + item.id + ".svg").mimeType("image/svg+xml").get(function (error, xml) {
+//       if (error) throw error;
+
+//       var importedNode = document.importNode(xml.documentElement, true);
+//       importedNode.id = item.id;
+//       svg
+//         .each(function () {
+//           this.appendChild(importedNode);
+//         })
+
+//       svg.select('svg#' + importedNode.id).call(tool_tip);
+//       svg.select('svg#' + importedNode.id)
+//         .attr("x", item.x)
+//         .attr("y", item.y)
+//         .attr("width", 100)
+//         .attr("height", 100)
+//         .on('mouseover', function () {
+//           svg.select('svg#' + importedNode.id)
+//             .style('cursor', 'pointer')
+//             .style('fill-opacity', 0.3);
+
+//           tool_tip.show(importedNode.id);
+//         })
+//         .on('mouseout', function () {
+//           svg.select('svg#' + importedNode.id)
+//             .style('fill-opacity', 1);
+//           tool_tip.hide();
+//         })
+
+//       //styleImportedSVG(importedNode.id);
+//     });
+//   })
+
+// function styleImportedSVG(c_id) {
+//   //console.log(svg.selectAll('svg'));
+//   // .filter(function(c_svg){ console.log(c_svg); return c_svg.id == c_id})
+//   // .attr("width", 50)
+//   // .attr("height", 50);
+//   // d3.selectAll('path').call(tool_tip);
+//   // d3.selectAll('path')
+//   //   .on('mouseover', function () {
+//   //     d3.selectAll('path')
+//   //       .style('cursor', 'pointer')
+//   //       .style('fill-opacity', 0.1);
+//   //     d3.selectAll('polygon')
+//   //       .style('cursor', 'pointer')
+//   //       .style('fill-opacity', 0.1);
+//   //     tool_tip.show();
+//   //   })
+//   //   .on('mouseout', function () {
+//   //     d3.selectAll('path')
+//   //       .style('fill-opacity', 1);
+//   //     d3.selectAll('polygon')
+//   //       .style('fill-opacity', 1);
+//   //     tool_tip.hide();
+//   //   })
+// }
