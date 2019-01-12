2e68ab84a505faee44690341e629fabc301d0355
selistym <selistym@protonmail.com>
2019-01-12 12:22:57 -0800

    zoom and boxzoom completed!

--- js/main.js
+++ js/main.js
@@ -4,29 +4,23 @@ var svg;
 w = 2293;
 h = 2139;
 
+var scale = 1.0;
+
 var block_names = ["right_pier", "center_hall", "front_pier", "left_pier", "main_hall"];
 
 // Create function to apply zoom to countriesGroup
 function zoomed() {
   t = d3.event.transform;
-
-  width = $("#map-holder").width();
-  height = $("#map-holder").height();
-
-  tx = Math.min(0, Math.max(t.x, w - w * t.k));
-  ty = Math.min(0, Math.max(t.y, h - h * t.k));
-
-  d3.zoomIdentity.translate([tx, ty]);
   levelGroup
-    .transition().duration(300)
-    .attr("transform", "translate(" + [tx, ty] + ")scale(" + t.k + ")");
-
+    .attr("transform", t);
 }
 
 // Define map zoom behaviour
 var zoom = d3
   .zoom()
   .scaleExtent([1, 4])
+  .translateExtent([[0, 0], [w, h]])
+  .extent([[0, 0], [w, h]])
   .on("zoom", zoomed);
 
 // on window resize
@@ -61,82 +55,57 @@ d3.xml("mapinfo/level0.svg").mimeType("image/svg+xml").get(function (error, xml)
 
   levelGroup = d3.select("g#level0");
 
+  bbox = levelGroup.node().getBBox();
+  vx = bbox.x;		// container x co-ordinate
+  vy = bbox.y;		// container y co-ordinate
+  vw = bbox.width;	// container width
+  vh = bbox.height;	// container height
+  defaultView = "" + vx + " " + vy + " " + vw + " " + vh;
+
+  svg
+    .attr("viewBox", defaultView)
+    .attr("preserveAspectRatio", "xMidYMid meet")
+
   block_names.forEach(b_name => {
 
     svg
       .select("g#" + b_name)
       .call(tool_tip)
       .on('mouseover', function () {
-        svg.select("g#" + b_name)
+        d3.select(this)
           .style('cursor', 'pointer')
           .style('fill-opacity', 0.5);
-        tool_tip.show(b_name);
+        //tool_tip.show(b_name);
       })
       .on('mouseout', function () {
-        svg.select("g#" + b_name)
+        d3.select(this)
           .style('fill-opacity', 1);
-        tool_tip.hide();
+        //tool_tip.hide();
       })
       .on('click', function () {
-        //console.log(svg.select("g#" + b_name).node().getBoundingClientRect());
-        //console.log(getBoundingBoxCenter(svg.select("g#" + b_name)));
-        //boxZoom(svg.select("g#" + b_name).node().getBBox(), getBoundingBoxCenter(svg.select("g#" + b_name)), 100);
+        if (d3.event.defaultPrevented) {
+          return; // panning, not clicking
+        }
+        node = d3.select(this);
+        var transform = getTransform(node, 2);
+
+        svg.transition().call(zoom.transform, d3.zoomIdentity.translate(transform.translate[0], transform.translate[1]).scale(transform.scale))
+        scale = transform.scale;
       });
 
   });
-  //initiateZoom();
 });
-// zoom to show a bounding box, with optional additional padding as percentage of box size
-function boxZoom(box, centroid, paddingPerc) {
 
-  // find size of block area defined
-  zoomWidth = Math.abs(w - box.width);//Math.abs(minXY[0] - maxXY[0]);
-  zoomHeight = Math.abs(h - box.height);//Math.abs(minXY[1] - maxXY[1]);
-
-  // find midpoint of block area defined
-  zoomMidX = centroid[0];
-  zoomMidY = centroid[1];
-  console.log(zoomMidX + ":" + zoomMidY)
-  // increase block area to include padding
-  zoomWidth = zoomWidth * (1 + paddingPerc / 100);
-  zoomHeight = zoomHeight * (1 + paddingPerc / 100);
-  // find scale required for area to fill svg  
-  maxXscale = $("svg").width() / zoomWidth;
-  maxYscale = $("svg").height() / zoomHeight;
-  zoomScale = Math.min(maxXscale, maxYscale);
-  // handle some edge cases
-  // limit to max zoom (handles tiny countries)
-  zoomScale = Math.min(zoomScale, maxZoom);
-  // limit to min zoom (handles large countries and countries that span the date line)
-  zoomScale = Math.max(zoomScale, minZoom);
-  // Find screen pixel equivalent once scaled  
-  offsetX = zoomScale * zoomMidX;
-  offsetY = zoomScale * zoomMidY;
-  // Find offset to centre, making sure no gap at left or top of holder
-  dleft = Math.min(0, $("svg").width() / 2);
-  dtop = Math.min(0, $("svg").height() / 2);
-  // Make sure no gap at bottom or right of holder
-  dleft = Math.max($("svg").width() - w * zoomScale, dleft);
-  dtop = Math.max($("svg").height() - h * zoomScale, dtop);
-  // set zoom
-  svg
-    .call(
-      zoom.transform,
-      d3.zoomIdentity.translate(zoomMidX, zoomMidY).scale(1.5)
-    );
+function getTransform(node, xScale) {
+  bbox = node.node().getBBox();
+  var bx = bbox.x;
+  var by = bbox.y;
+  var bw = bbox.width;
+  var bh = bbox.height;
+  var tx = -bx * xScale + vx + vw / 2 - bw * xScale / 2;
+  var ty = -by * xScale + vy + vh / 2 - bh * xScale / 2;
+  return { translate: [tx, ty], scale: xScale }
 }
-//get selected block's centroid position
-function getBoundingBoxCenter(selection) {
-  // get the DOM element from a D3 selection
-  // you could also use "this" inside .each()
-  var element = selection.node();
-  // use the native SVG interface to get the bounding box
-  var bbox = element.getBBox();
-  // return the center of the bounding box
-  return [bbox.x + bbox.width / 2, bbox.y + bbox.height / 2];
-}
-
-
 
 //handler for zoom home/in/out
 $('#zoom-home').on('click', function () {
